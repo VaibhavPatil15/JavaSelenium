cc34de44cb25370824f18da723175ae8a0e67922
VaibhavPatil15 <vp671291@gmail.com>
2025-02-01 21:46:28 +0530

    dfgdfg

--- src/Test/automate.java
+++ src/Test/automate.java
@@ -3,7 +3,7 @@ package Test;
 public class automate {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, Vaibhav here ");
+		System.out.println("HelloWorld, Vaibhav here ");
 
 	}
 
